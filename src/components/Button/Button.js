ba36f982f8c8c961815a0609d7dcda15f2f1d5ed
sdarmaputra <surya.igede@gmail.com>
2018-04-24 23:01:11 +0700

    add Button component - color variety - icon - gap - blocked button - filled button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import chroma from 'chroma-js'
 
 export default class Button extends Component {
 	constructor(props) {
 		super(props)
+		this.availableColors = ['default', 'blue', 'red']
 		this.state = {
 			hovered: false
 		}
@@ -17,30 +20,110 @@ export default class Button extends Component {
 	handleMouseLeave(event) {
 		if (this.state.hovered) this.setState({ hovered: false })
 	}
+
+	generateIconStyle({ iconPosition, iconSize }) {
+		const availablePositions = ['left', 'top', 'right']
+		const availableSizes = {
+			default: '16px',
+			large: '28px'
+		}
+		const selectedPosition = availablePositions.includes(iconPosition) 
+			? iconPosition
+			: 'left'
+		const selectedSize = iconSize in availableSizes ? availableSizes[iconSize] : availableSizes.default
+		return Object.assign(
+			{},
+			{ fontSize: selectedSize },
+			selectedPosition === 'left' ? { float: 'left', marginRight: '10px' } : {},
+			selectedPosition === 'right' ? { float: 'right', marginLeft: '10px' } : {},
+			selectedPosition === 'top' ? { display: 'block', width: '100%' } : {},
+		)
+	}
 	
 	render() {
-		const { text } = this.props
-		const mergedStyle = Object.assign({}, styles.default, this.state.hovered ? styles.hovered : {})
+		const { text, onClick, block, fill, color, gap, focus, icon } = this.props
+		const selectedColor = this.availableColors.includes(color) ? styles[color] : styles.default
+		const mergedStyle = Object.assign(
+			{}, 
+			styles.button,
+			selectedColor,
+			block ? styles.block : {},
+			fill ? styles.fill : {},
+			gap ? styles.gap : {},
+			focus ? { background: chroma(selectedColor.background).darken(0.15) } : {},
+			this.state.hovered ? { background: chroma(selectedColor.background).darken(0.2) } : {},
+		)
+		const iconStyle = icon ? this.generateIconStyle(this.props) : {}
+
 		return (
 			<button
 				style={ mergedStyle }
 				onMouseEnter={ this.handleMouseEnter }
-				onMouseLeave={ this.handleMouseLeave }>
+				onMouseLeave={ this.handleMouseLeave }
+				onClick={ onClick }>
+				{ icon ? <div style={ iconStyle }>{ icon }</div> : null }
 				{ text }
 			</button>
 		)
 	}
 }
 
+Button.propTypes = {
+	text: PropTypes.string,
+	onClick: PropTypes.func,
+	block: PropTypes.bool,
+	fill: PropTypes.bool,
+	color: PropTypes.string,
+	gap: PropTypes.bool,
+	focus: PropTypes.bool,
+	icon: PropTypes.any,
+	iconPosition: PropTypes.string,
+	iconSize: PropTypes.string
+}
+
+Button.defaultProps = {
+	text: 'Submit',
+	onClick: function() {},
+	block: false,
+	fill: false,
+	color: 'default',
+	gap: false,
+	focus: false,
+	icon: false,
+	iconPosition: 'left',
+	iconSize: 'default'
+}
+
 const styles = {
-	default: {
-		background: ' #5dade2',
+	button: {
 		border: 'none',
-		color: '#fff',
+		boxSizing: 'border-box',
 		cursor: 'pointer',
+		fontSize: '16px',
 		padding: '8px 15px'
 	},
-	hovered: {
-		background: '#2471a3'
+	block: {
+		display: 'block',
+		width: '100%'
+	},
+	fill: {
+		display: 'block',
+		height: '100%',
+		width: '100%'
+	},
+	gap: {
+		margin: '5px'
+	},
+	default: {
+		background: '#fcfcfc',
+		color: '#888',
+	},
+	blue: {
+		background: '#37B8D5',
+		color: '#fff'
+	},
+	red: {
+		background: '#E3475E',
+		color: '#fff'
 	}
 }
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,7 +1,50 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import Icon from 'react-icons-kit'
+import { home } from 'react-icons-kit/icomoon/home'
+import { pencil } from 'react-icons-kit/icomoon/pencil'
+import { equalizer2 } from 'react-icons-kit/icomoon/equalizer2'
 
 import Button from './Button'
 
 storiesOf('Button', module)
-	.add('default', () => <Button text='Test' />)
+	.add('standard', () => <Button text='Hey! I am a button :)' color='blue' />)
+	.add('blocked', () => <Button text='I am a huge button' color='blue' block={true} />)
+	.add('with colors', () =>
+		<div>
+			<h6>Button without gap</h6>
+			<Button text='Default' color='default' />
+			<Button text='Blue' color='blue' />
+			<Button text='Red' color='red' />
+			
+			<h6>Button with gap</h6>
+			<Button text='Default' color='default' gap={true} />
+			<Button text='Blue' color='blue' gap={true} />
+			<Button text='Red' color='red' gap={true} />
+		</div>
+	)
+	.add('with focus', () => 
+		<div>
+			<Button text='Default' color='default' />
+			<Button text='Default Focused' color='default' focus={true} />
+			<br />
+			<Button text='Blue' color='blue' />
+			<Button text='Blue Focused' color='blue' focus={true} />
+			<br />
+			<Button text='Red' color='red' />
+			<Button text='Red Focused' color='red' focus={true} />
+		</div>
+	)
+	.add('with icon', () =>
+		<div>
+			<Button text='Left Icon' color='default' gap={true} icon={ <Icon icon={ home } /> } iconPosition='left'/>
+			<Button text='Right Icon' color='blue' gap={true} icon={ <Icon icon={ pencil } /> } iconPosition='right'/>
+			<Button text='Top Icon' color='red' gap={true} icon={ <Icon icon={ equalizer2 } /> } iconPosition='top' />
+		</div>		
+	)
+	.add('with callback', () => 
+		<Button 
+			text="Click Me!" 
+			color='blue' 
+			onClick={() => alert('Hey! You clicked me')}/>
+	)
