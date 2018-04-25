243ba1432685be06844821d1ecb4b40ce6e86014
sdarmaputra <surya.igede@gmail.com>
2018-04-25 10:12:32 +0700

    add transparancy and block

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -30,23 +30,42 @@ export default class Dropdown extends Component {
 	}
 
 	getItems() {
-		const { items } = this.props
-		const finalItems = [
+		const { items, placeholder } = this.props
+		const finalItems = placeholder ? [
 			'- Select one -',
 			...items
-		]
+		] : items
 		return finalItems
 	}
 
 	render() {
+		const { block, fill, width, transparentOnClosed, colorWhenTransparent } = this.props
 		const items = this.getItems()
 		const selectedText = items[this.state.selectedIndex] 
-		const dropdownStyles = Object.assign({}, styles.dropdown, this.state.isOpen ? styles.dropdownOpened : {})
+		const dropdownStyles = Object.assign(
+			{}, 
+			styles.dropdown, 
+			!this.state.isOpen && transparentOnClosed ? { background: 'transparent' } : {},
+			this.state.isOpen ? styles.dropdownOpened : {},
+			width ? { width } : {},
+			block ? styles.block : {}
+		)
+		const placeHolderStyles = Object.assign(
+			{},
+			styles.item,
+			!this.state.isOpen && transparentOnClosed ? { color: colorWhenTransparent } : {}
+		)
+		const caretStyles = Object.assign(
+			{},
+			styles.caret,
+			!this.state.isOpen && transparentOnClosed ? { color: colorWhenTransparent } : {}
+		)
+
 		return (
 			<div style={ dropdownStyles }>
-				<div style={ styles.caret }>&#9662;</div>
+				<div style={ caretStyles }>&#9662;</div>
 				<div 
-					style={ styles.item } 
+					style={ placeHolderStyles } 
 					onClick={ this.toggle }>
 					{ selectedText }
 				</div>
@@ -67,12 +86,22 @@ export default class Dropdown extends Component {
 
 Dropdown.propTypes = {
 	items: PropTypes.array,
-	onItemClick: PropTypes.func
+	onItemClick: PropTypes.func,
+	placeholder: PropTypes.bool,
+	block: PropTypes.bool,
+	width: PropTypes.number,
+	transparentOnClosed: PropTypes.bool,
+	colorWhenTransparent: PropTypes.string
 }
 
 Dropdown.defaultProps = {
 	items: [],
-	onItemClick: function() {}
+	onItemClick: function() {},
+	placeholder: true,
+	block: false,
+	width: 200,
+	transparentOnClosed: false,
+	colorWhenTransparent: '#888'
 }
 
 class Item extends Component {
@@ -119,12 +148,15 @@ const styles = {
 		boxSizing: 'border-box',
 		cursor: 'pointer',
 		minHeight: commons.height,
-		position: 'relative',
-		transition: 'box-shadow 0.15s ease-in'
+		position: 'absolute',
+		transition: 'background, box-shadow 0.15s ease-in'
 	},
 	dropdownOpened: {
 		border: '1px solid #efefef',
 		boxShadow: '0 2px 2px 2px #efefef'
+	},
+	block: {
+		width: '100%',
 	},
 	caret: {
 		color: '#999',
